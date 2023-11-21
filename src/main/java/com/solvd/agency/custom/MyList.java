0264d02e5b39d8df716841965e317d1b11f5b2ad
jakubg123 <jakubgniadek87@gmail.com>
2023-11-21 19:53:22 +0100

    3) fix some bisness logic 5) add working with all interfacces

--- src/main/java/com/solvd/agency/custom/MyList.java
+++ src/main/java/com/solvd/agency/custom/MyList.java
@@ -1,18 +1,21 @@
 package com.solvd.agency.custom;
 
-import java.util.Collection;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
 
-public class MyList<T> {
+import java.util.*;
+
+public class MyList<T> implements List<T> {
 
     private Node<T> head;
+    private int size;
 
 
-    public void add(T data) {
+    @Override
+    public boolean add(T data) {
         Node<T> newNode = new Node<>(data);
         if (this.head == null) {
             head = newNode;
+            return false;
         } else {
             Node<T> current = head;
             while (current.next != null) {
@@ -20,7 +23,10 @@ public class MyList<T> {
             }
 
             current.next = newNode;
+
         }
+        size++;
+        return true;
     }
 
     public void display(){
@@ -31,6 +37,9 @@ public class MyList<T> {
         }
     }
 
+
+
+    @Override
     public int size() {
         if (head == null)
             return 0;
@@ -43,6 +52,10 @@ public class MyList<T> {
         return counter;
     }
 
+    @Override
+    public boolean isEmpty(){
+        return size == 0;
+    }
     public void clear() {
         this.head = null;
     }
@@ -59,6 +72,54 @@ public class MyList<T> {
         }
 
         return result;
+    }
+
+    @Override
+    public T get(int i) {
+        return null;
+    }
+
+    @Override
+    public T set(int i, T t) {
+        return null;
+    }
+
+    @Override
+    public void add(int i, T t) {
+
+    }
+
+    @Override
+    public T remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @NotNull
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public List<T> subList(int i, int i1) {
+        return null;
     }
 
     @Override
@@ -94,6 +155,11 @@ public class MyList<T> {
         return removedAll;
     }
 
+    @Override
+    public boolean retainAll(@NotNull Collection<?> collection) {
+        return false;
+    }
+
     public boolean remove(Object o){
         if(o == null)
             throw new NoSuchElementException("No such an element");
@@ -112,18 +178,25 @@ public class MyList<T> {
             else {
                 previous.next = current.next;
             }
-            return true;
         }
-        return false;
+        size--;
+        return true;
     }
 
-    boolean addAll(Collection<? extends T> c)
+    @Override
+    public boolean addAll(Collection<? extends T> c)
     {
-        for(T o : c){
-            add(o);
+        for(T element  : c){
+            add(element);
         }
         return true;
     }
+
+    @Override
+    public boolean addAll(int i, @NotNull Collection<? extends T> collection) {
+        return false;
+    }
+
     public boolean contains(Object o) {
         Node<T> current = head;
         while (current != null) {
@@ -134,6 +207,25 @@ public class MyList<T> {
         }
         return false;
     }
+
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @NotNull
+    @Override
+    public <T1> T1[] toArray(@NotNull T1[] t1s) {
+        return null;
+    }
+
     public boolean containsAll(Collection<?> c) {
         for (Object element : c) {
             if (!contains(element)) {
